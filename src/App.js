6381993e651383927d765be2c04b4745d5a4b079
Gurpreet Singh <gsbedi90@gmail.com>
2024-09-26 00:16:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import bgImage from './assets/background.jpg'
+import styles from './styles/app.module.css'
+import { IoIosMail } from "react-icons/io";
+import pic from './assets/akal.jpg'
+// import pic from './assets/'
+// Maharaja immigration and consultants pvt ltd.
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main className={styles.mainBody}>
+        <div className={styles.headerSection}>
+          <div className={styles.headerContent}>
+            <span>Easy & Simple Visa Processing Agency</span>
+          </div>
+          <img src={bgImage} alt='background' />
+        </div>
+        <h3>About Us</h3>
+        <div className={styles.aboutus}>
+          <div className={styles.dirPic}>
+            <img src={pic} alt='director'></img>
+          </div>
+          <div className={styles.content}>
+            Akalpreet Singh is an immigration professional with 8-9 years of experience in the field. With an MBA, they bring a strong understanding of both immigration processes and business strategies. Akalpreet Singh is dedicated to helping clients navigate complex immigration challenges with confidence and ease. Their expertise ensures that clients receive tailored solutions to meet their needs.
+          </div>
+
+        </div>
+        <footer className={styles.footer}>
+          <p style={{ textAlign: 'center' }}>Quick Link</p>
+          <div className={styles.gmail}>
+            <IoIosMail style={{ fontSize: '1.3rem' }} />
+            <span style={{ fontSize: '.7rem' }}>Maharajaimmigration1@gmail.com</span>
+          </div>
+          <p style={{ fontSize: '.8rem', fontWeight: '300', textAlign: 'center' }}>© 2024 Maharaja immigration and Consultants PVT LTD. All rights reserved.</p>
+        </footer>
+      </main>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import styles from './navbar.module.css'
+import logo from '../assets/White Black Elegant Concept Football Club Logo (1)_page-0001.jpg'
+
+function Navbar() {
+    return (
+        <div className={styles.main}>
+            <img className={styles.logo} src={logo} alt='logo' />
+            <span className={styles.heading}>Maharaha Immigration</span>
+        </div>
+    )
+}
+
+export default Navbar
